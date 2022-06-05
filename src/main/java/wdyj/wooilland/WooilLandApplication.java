d6376b0934422b81e8a31ced7b9302173341827d
wooil <wooil@mz.co.kr>
2022-06-05 15:49:56 +0900

    Initial commit

---
+++ src/main/java/wdyj/wooilland/WooilLandApplication.java
@@ -0,0 +1,13 @@
+package wdyj.wooilland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooilLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooilLandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wdyj/wooilland/WooilLandApplicationTests.java
@@ -0,0 +1,13 @@
+package wdyj.wooilland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooilLandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
